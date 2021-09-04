e6be38f362374c09a7b1ac804db1b88ef1185d7d
Никитка <nikita79134751837@mail.ru>
2021-09-04 19:12:31 +0700

    ArrayListHome. Исправил замечания

--- ArrayListHome/src/ru/academits/popov/arraylisthome/Main.java
+++ ArrayListHome/src/ru/academits/popov/arraylisthome/Main.java
@@ -1,42 +1,57 @@
 package ru.academits.popov.arraylisthome;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("ArrayListHome/wholeNumber.txt"))) {
+        ArrayList<String> fileLine = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("ArrayListHome/input.txt"))) {
             String line;
 
             while ((line = bufferedReader.readLine()) != null) {
-                arrayList.add(line);
+                fileLine.add(line);
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Ошибка чтения из файла: " + e.getMessage());
         }
 
-        try {
-            for (int i = 0; i < arrayList.size(); ++i) {
-                if (Integer.parseInt(arrayList.get(i)) % 2 == 0) {
-                    arrayList.remove(arrayList.get(i));
-                }
-            }
+        System.out.println("Список сторок, прочитанных из файла = " + fileLine);
 
-            System.out.println("Список без четных чисел = " + arrayList);
-        } catch (NumberFormatException e) {
-            System.out.println("Неудается выполнить удаление четных чисел, потому что в файле находятся символы");
-        }
+        ArrayList<Integer> arrayList1 = new ArrayList<>(Arrays.asList(489, 578, 999, 666, 777, 253, 7, 2));
 
-        ArrayList<String> uniqueList = new ArrayList<>();
-        for (String s : arrayList) {
-            if (!uniqueList.contains(s)) {
-                uniqueList.add(s);
+        Iterator<Integer> iterator = arrayList1.iterator();
+
+        System.out.println("Список чисел до удаления четных = " + arrayList1);
+
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove();
             }
         }
 
-        System.out.println("Список с уникальными значениями = " + uniqueList);
+        System.out.println("Список без четных чисел = " + arrayList1);
+
+        ArrayList<Integer> arrayList2 = new ArrayList<>(Arrays.asList(1, 4, 489, 4, 1, 578, 999, 999, 666, 777, 2, 253, 7, 2));
+
+        System.out.println("Список чисел до удаления четных = " + arrayList2);
+
+        ArrayList<Integer> uniqueNumbersList = new ArrayList<>();
+
+        for (int number : arrayList2) {
+            if (!uniqueNumbersList.contains(number)) {
+                uniqueNumbersList.add(number);
+            }
+        }
+
+        System.out.println("Список с уникальными значениями = " + uniqueNumbersList);
     }
 }

85ce16d6eb0d2e6fcf0314f304990e04c1e83bac
Никитка <nikita79134751837@mail.ru>
2021-10-20 09:11:17 +0700

    Lambda. Исправил

--- Lambda/src/ru/academits/popov/lambda_main/Main.java
+++ Lambda/src/ru/academits/popov/lambda_main/Main.java
@@ -2,40 +2,52 @@ package ru.academits.popov.lambda_main;
 
 import ru.academits.popov.person.Person;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Person> persons = new ArrayList<>();
-        persons.add(new Person("Иван", 42));
-        persons.add(new Person("Сергей", 12));
-        persons.add(new Person("Анатолий", 26));
-        persons.add(new Person("Иван", 35));
-        persons.add(new Person("Анна", 10));
-        persons.add(new Person("Игорь", 34));
-        persons.add(new Person("Степан", 25));
-        persons.add(new Person("Константин", 78));
-        persons.add(new Person("Алина", 62));
-        persons.add(new Person("Игорь", 43));
+        ArrayList<Person> persons = new ArrayList<>(Arrays.asList(
+                new Person("Иван", 42),
+                new Person("Сергей", 12),
+                new Person("Анатолий", 26),
+                new Person("Иван", 35),
+                new Person("Анна", 10),
+                new Person("Игорь", 34),
+                new Person("Степан", 25),
+                new Person("Константин", 78),
+                new Person("Алина", 62),
+                new Person("Игорь", 43)
+        ));
 
-        List<String> distinctNames = persons.stream().map(Person::getName).distinct().collect(Collectors.toList());
+        List<String> distinctNames = persons.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.toList());
         System.out.println("Список уникальных имен: " + distinctNames);
 
-        String distinctNameLine = persons.stream().map(Person::getName).distinct().collect(Collectors.joining(", ", "Имена: ", ""));
-        System.out.println(distinctNameLine);
+        String distinctNamesLine = persons.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.joining(", ", "Имена: ", "."));
+        System.out.println(distinctNamesLine);
 
-        List<Person> personUnder18 = persons.stream().filter(x -> x.getAge() < 18).collect(Collectors.toList());
+        List<Person> personsUnder18 = persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .collect(Collectors.toList());
 
-        Double personUnder18AverageAge = personUnder18.stream().collect(Collectors.averagingInt(Person::getAge));
-        System.out.println("Средний возраст людей до 18 = " + personUnder18AverageAge);
+        Double personsUnder18AverageAge = personsUnder18.stream()
+                .collect(Collectors.averagingInt(Person::getAge));
+        System.out.println("Средний возраст людей до 18 = " + personsUnder18AverageAge);
 
-        Map<String, Double> ageByName = persons.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
+        Map<String, Double> ageByName = persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
         System.out.println("Средний возраст на имя: " + ageByName);
 
-        List<Person> personByAge20To45 = persons.stream().filter(x -> x.getAge() > 20 && x.getAge() < 45).sorted((x, y) -> x.getAge() - y.getAge()).collect(Collectors.toList());
-        System.out.println(personByAge20To45);
+        List<Person> personsByAge20To45 = persons.stream()
+                .filter(p -> p.getAge() >= 20 && p.getAge() <= 45)
+                .sorted((p1, p2) -> (p2.getAge() - p1.getAge()))
+                .collect(Collectors.toList());
+        System.out.println(personsByAge20To45);
     }
 }
--- Lambda/src/ru/academits/popov/person/Person.java
+++ Lambda/src/ru/academits/popov/person/Person.java
@@ -4,21 +4,21 @@ public class Person {
     private final String name;
     private final int age;
 
-    public Person(String name, int age){
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Name: " + name + ", age: " + age;
     }
 }
